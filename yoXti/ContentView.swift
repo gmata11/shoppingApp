d7def37500a7d85ccb252fe0cf18a8fe3c0ddd32
Gerard <gerardmc12@gmail.com>
2020-07-15 12:56:30 +0200

    Initial Commit

---
+++ yoXti/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  yoXti
+//
+//  Created by Gerard Mata Carrera on 15/07/2020.
+//  Copyright © 2020 Gerard Mata Carrera. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ yoXtiTests/yoXtiTests.swift
@@ -0,0 +1,34 @@
+//
+//  yoXtiTests.swift
+//  yoXtiTests
+//
+//  Created by Gerard Mata Carrera on 15/07/2020.
+//  Copyright © 2020 Gerard Mata Carrera. All rights reserved.
+//
+
+import XCTest
+@testable import yoXti
+
+class yoXtiTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
