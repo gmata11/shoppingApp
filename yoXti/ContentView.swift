3e358316f950e8d97764fa4801826b038ea2ed5b
Gerard <gerardmc12@gmail.com>
2020-07-15 15:21:52 +0200

    Creada estructura y menu

--- yoXti/ContentView.swift
+++ yoXti/ContentView.swift
@@ -10,12 +10,80 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 15) {
+            ZStack {
+                Text("YO POR TI").font(.body)
+                HStack(spacing: 18) {
+                    Button(action: {
+                        //Todo
+                    }) {
+                        Image("menu").resizable()
+                        .frame(width: 20, height: 20)
+                    }
+                    Spacer()
+                    
+                    Button(action: {
+                        //Todo
+                    }) {
+                        Image("search").resizable()
+                        .frame(width: 20, height: 20)
+//                            .foregroundColor(.black)
+                    }
+                    
+                    Button(action: {
+                        //Todo
+                    }) {
+                        Image("noti").resizable()
+                        .frame(width: 20, height: 20)
+                    }
+                    
+                    Button(action: {
+                        //Todo
+                    }) {
+                        Image("shop").resizable()
+                        .frame(width: 20, height: 20)
+                    }
+                }
+            }.background(Color.white).padding([.leading,.trailing,.top], 15)
+            
+            MainView()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct MainView: View {
+    var body: some View {
+        VStack(spacing: 15) {
+            HStack {
+                HStack {
+                    Button(action: {
+                        //Todo
+                    }) {
+                        HStack {
+                            Text("One")
+                            Spacer()
+                            Image("down").resizable()
+                            .frame(width: 20, height: 20)
+                        }.padding()
+                    }.foregroundColor(.black)
+                        .background(Color.white)
+                    Button(action: {
+                        //ToDo
+                    }) {
+                        Image("filter").resizable()
+                        .frame(width: 20, height: 20)
+                    }.background(Color.white)
+                }
+            }
+            Spacer()
+        }
+        .background(Color("Color"))
+        .padding()
     }
 }
