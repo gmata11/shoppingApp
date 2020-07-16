8c5d1c8bd514579f150bacda502e0e7d98c13071
Gerard <gerardmc12@gmail.com>
2020-07-16 16:07:46 +0200

    Working navigation

--- yoXti/ContentView.swift
+++ yoXti/ContentView.swift
@@ -11,36 +11,40 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        VStack(spacing: 15) {
-            ZStack {
-                Image("yoPorTiLogoPng").resizable()
-                .frame(width: 60, height: 25)
-                HStack(spacing: 18) {
-                    Button(action: {
-                        //Todo
-                    }) {
-                        Image("menu").resizable()
-                        .frame(width: 20, height: 20)
-                    }
-                    Spacer()
+        NavigationView {
+            VStack(spacing: 15) {
+                ZStack {
+                    Image("yoPorTiLogoPng").resizable()
+                    .frame(width: 60, height: 25)
+                    HStack(spacing: 18) {
+                        Button(action: {
+                            //Todo
+                        }) {
+                            Image("menu").resizable()
+                            .frame(width: 20, height: 20)
+                        }
+                        Spacer()
 
-                    Button(action: {
-                        //Todo
-                    }) {
-                        Image("search").resizable()
-                        .frame(width: 20, height: 20)
-                    }
+                        Button(action: {
+                            //Todo
+                        }) {
+                            Image("search").resizable()
+                            .frame(width: 20, height: 20)
+                        }
 
-                    Button(action: {
-                        //Todo
-                    }) {
-                        Image("shop").resizable()
-                        .frame(width: 20, height: 20)
+                        Button(action: {
+                            //Todo
+                        }) {
+                            Image("shop").resizable()
+                            .frame(width: 20, height: 20)
+                        }
                     }
-                }
-            }.background(Color.white).padding([.leading,.trailing,.top], 15)
+                }.background(Color.white).padding([.leading,.trailing,.top], 15)
 
-            MainView()
+                MainView()
+            }.navigationBarBackButtonHidden(true)
+            .navigationBarHidden(true)
+            .navigationBarTitle("")
         }
     }
 }
@@ -83,31 +87,81 @@ struct MainView: View {
 }
 
 struct DetailsScroll: View {
+    @State var show = false
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             VStack(spacing: 12) {
                 ForEach(datas) {i in
                     HStack {
                         ForEach(i.rows) {j in
-                            VStack(spacing: 8) {
-                                Image(j.image).resizable().frame(width: UIScreen.main.bounds.width / 2 - 25, height: 160)
-                                HStack {
-                                    VStack(alignment: .leading, spacing: 10) {
-                                        Text(j.name)
-                                        Text(j.price).fontWeight(.heavy)
-                                    }
-                                    Spacer()
-                                    Button(action: {
-                                        //Todo
-                                    }) {
-                                        Image("option").resizable()
-                                        .frame(width: 20, height: 20)
-                                        .foregroundColor(.black)
-                                    }.padding(.trailing, 15)
-                                }
-                            }
+                            CardsView(row: j)
                         }
                     }
+                }
+            }
+        }
+    }
+}
+
+struct DetailView: View {
+    @Binding var show: Bool
+    var body: some View {
+        VStack {
+            ZStack {
+            Image("yoPorTiLogoPng").resizable()
+            .frame(width: 60, height: 25)
+            HStack(spacing: 18) {
+                Button(action: {
+                    self.show.toggle()
+                }) {
+                    Image("back").resizable()
+                    .frame(width: 10, height: 10)
+                }
+                Spacer()
+
+                Button(action: {
+                    //Todo
+                }) {
+                    Image("search").resizable()
+                    .frame(width: 20, height: 20)
+                }
+
+                Button(action: {
+                    //Todo
+                }) {
+                    Image("shop").resizable()
+                    .frame(width: 20, height: 20)
+                }
+            }.navigationBarHidden(true).navigationBarBackButtonHidden(true).navigationBarTitle("")
+            }
+
+            Spacer()
+        }.padding()
+    }
+}
+
+struct CardsView: View {
+    @State var show = false
+    var row: row
+    var body: some View {
+        VStack {
+            VStack(spacing: 8) {
+                NavigationLink(destination: DetailView(show: $show), isActive: $show) {
+                    Image(row.image).renderingMode(.original).resizable().frame(width: UIScreen.main.bounds.width / 2 - 25, height: 160)
+                }
+                HStack {
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text(row.name).fontWeight(.medium)
+                        Text(row.price).fontWeight(.heavy)
+                    }
+                    Spacer()
+                    Button(action: {
+                        //Todo
+                    }) {
+                        Image("option").resizable()
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(.black)
+                    }.padding(.trailing, 15)
                 }
             }
         }
@@ -119,13 +173,17 @@ struct Splash: View {
     @State var isActive = false
     
     var body: some View {
-        ZStack {
+        ZStack() {
             if self.isActive {
                 ContentView()
             }else {
                 Color.white.edgesIgnoringSafeArea(.all)
                 ZStack {
                     Image("yoPorTiLogoPng").rotation3DEffect(.degrees(360), axis: (x: 1, y: 0, z: 0))
+                    
+                }
+                ZStack {
+                    Text("Created by gmata").frame(width: 150, height: 700, alignment: .bottomLeading)
                 }
             }
             
