fca26312662985b632fcaca07372b692373fb3ee
Gerard <gerardmc12@gmail.com>
2020-07-22 19:05:58 +0200

    Added files

---
+++ yoXti/MenuViews/DropDownMenu.swift
@@ -0,0 +1,21 @@
+//
+//  DropDownMenu.swift
+//  Yo Por Ti
+//
+//  Created by Gerard Mata Carrera on 22/07/2020.
+//  Copyright © 2020 Gerard Mata Carrera. All rights reserved.
+//
+
+import SwiftUI
+
+struct DropDownMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DropDownMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        DropDownMenu()
+    }
+}
